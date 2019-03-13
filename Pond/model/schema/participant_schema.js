b209d8728dd1ea07738cbbbc8e3ebbaee52541a0
Arup A <arup.arcalgud@gmail.com>
2019-03-13 14:32:19 -0400

    profilepic in schema
    can we store profile pic as a url?

--- Pond/model/schema/participant_schema.js
+++ Pond/model/schema/participant_schema.js
@@ -11,7 +11,8 @@ exports.participant_schema = new mongoose_1.Schema({
     userName: String,
     date: Date,
     time: Number,
-    matched: Boolean
+    matched: Boolean,
+    //profilepic: String //profile pic will be stored as a binary string?
 }, {
     //collection: process.env.MongoDbCollectionAccount,
     collection: "ParticipantSchema"
